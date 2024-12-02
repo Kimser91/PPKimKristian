a0c2a82d8b5721f8e56ebed6a65ad3a2f9b0cef5
KristianHeggdal <kristianheggdal@hotmail.no>
2024-12-02 13:18:55 +0100

    adsfadf

--- PPKimKristian/Main.cs
+++ PPKimKristian/Main.cs
@@ -2,5 +2,23 @@
 
 public class Main
 {
-    
+    private List<Movie> movies;
+
+    public Main()
+    {
+        movies = new List<Movie>()
+        {
+            new Movie("Lord of the rings", "2001", "Awesome movie", "Adventure", "Peter Jackson"),
+            new Movie("Star Wars", "1974","Cool movie","Fantasy", "George Lucas"),
+
+        };
+
+        void ShowMovies()
+        {
+            for (int i = 0; i < movies.Count; i++)
+            {
+                Console.WriteLine(movies[i].Title + movies[i].ReleaseYear + movies[i].Description + movies[i].Genre);
+            }
+        }
+    }
 }
--- PPKimKristian/Movie.cs
+++ PPKimKristian/Movie.cs
@@ -2,5 +2,21 @@
 
 public class Movie
 {
-    fsdf
+   public string Title { get; set; }
+   public string ReleaseYear { get; set; }
+   public string Description { get; set; }
+   public string Genre { get; set; }
+   public string Director { get; set; }
+
+   public Movie(string title, string releaseYear, string description, string genre, string director)
+   {
+       Title = title;
+       ReleaseYear = releaseYear;
+       Description = description;
+       Genre = genre;
+       Director = director;
+   }
+
+
 }
+
